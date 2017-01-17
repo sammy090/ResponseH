ddeeb4bf5cf6ae64e30b207216b08705fbf0378c
Suraj Igure <surajigure08@gmail.com>
2017-01-17 13:26:47 -0800

    HomePageChanges

--- src/test/java/TestCases/HomePageResponse.java
+++ src/test/java/TestCases/HomePageResponse.java
@@ -18,6 +18,11 @@ public class HomePageResponse extends HomePageHeaders {
         new HomePageResponse().HomePageResponseForUSA();
     }
 
+    @Test
+    public void TestHime() throws Exception {
+
+    }
+
 }
 
 
